63b4f3d8efa9f1929216d2c9f0bd6366ecc5dd43
dISCOeRG0sUM <49689599+dISCOeRG0sUM@users.noreply.github.com>
2019-06-20 22:25:17 +0200

    - Läuft jetzt mit Syphone IN + OUT bei 40fps - Syphon OUt nur noch die Metaballs - MIDI fliegt raus da wire alls per OSC kontrolieren wollen.

--- src/main/java/RembrandMeta.java
+++ src/main/java/RembrandMeta.java
@@ -1,8 +1,9 @@
-import codeanticode.syphon.*;
+import codeanticode.syphon.SyphonClient;
+import codeanticode.syphon.SyphonServer;
 import processing.core.PApplet;
+import processing.core.PGraphics;
 import processing.core.PImage;
 import processing.core.PVector;
-import themidibus.MidiBus;
 
 
 
@@ -12,17 +13,13 @@ public class RembrandMeta extends PApplet{
         PApplet.main("RembrandMeta");
     }
 
-    MidiBus myMidi;
     MetaBalls mBalls;
     SyphonClient client;
     SyphonServer server;
     PImage img;
 
+    PGraphics sendSypongImg;
 
-    //** Hintergrund Bilder
-    PImage [] myPix;
-    int indexPix = 0;
-    int numOfPix = 5;
 
     // für dir Forces
     float masse = 1; //masse der Metaballs
@@ -42,23 +39,11 @@ public class RembrandMeta extends PApplet{
         // die Metaballs werden initialisiertÅ
         mBalls = new MetaBalls(this);
 
-        // ** Midi
-        // Gibt alle verfügbaren Midi Devices aus
-        MidiBus.list();
-        myMidi = new MidiBus(this,0,1);
-
         // ** Syphong
         client = new SyphonClient(this);
         server = new SyphonServer(this, "Processing Syphon");
 
-
-
-        // ** Hintergrund Bilder laden
-        myPix = new PImage[numOfPix];
-        for (int i = 0; i < numOfPix; i++){
-            myPix[i] = loadImage("Pix/" +i +".png", "png");
-        }
-
+        sendSypongImg = createGraphics(width, height,P2D);
 
 
 
@@ -66,32 +51,36 @@ public class RembrandMeta extends PApplet{
 
     public void draw(){
 
-        background(0);
+        sendSypongImg.beginDraw();
+        sendSypongImg.background(0);
 
-        // HIntergrund Bilder anzeigen
-        indexPix = constrain(indexPix,0,numOfPix -1);
-        image(myPix[indexPix],0,0);
+        // ** Metaballs anzeigen
+        sendSypongImg.image(mBalls.show(), 0, 0);  // wie kann ich das hier in das sendSyponImag bekommen?
+        mBalls.update();
 
-        if (client.newFrame()) {
-            // The first time getImage() is called with
-            // a null argument, it will initialize the PImage
-            // object with the correct size.
-            img = client.getImage(img); // load the pixels array with the updated image info (slow)
-            //img = client.getImage(img, false); // does not load the pixels array (faster)
-        }
-        if (img != null) {
-            image(img, 0, 0, width, height);
-        }
+        sendSypongImg.endDraw();
+        image(sendSypongImg,0,0);
 
-        // Metaballs anzeigen
-        mBalls.show();
+        // ** Syphon send imag
+        server.sendImage(sendSypongImg);
 
-        // füge Krafte hin zu
+        // ** füge Krafte hin zu
         mBalls.applyForces(wind);
         mBalls.applyForces(gravity);
 
 
-
+        // ** Syphon get image
+        if (client.newFrame()) {
+            // The first time getImage() is called with
+            // a null argument, it will initialize the PImage
+            // object with the correct size.
+            //img = client.getImage(img); // load the pixels array with the updated image info (slow)
+            img = client.getImage(img, false); // does not load the pixels array (faster)
+        }
+        if (img != null) {
+            tint(255,120);
+            image(img, 0, 0, width, height);
+        }
 
 
         //** FrameRate kontrolle und Warnung
@@ -102,78 +91,11 @@ public class RembrandMeta extends PApplet{
         }
         textSize(14);
         text("FrameRate: " + frameRate, 20, height - 20);
-
-        server.sendScreen();
+        // **
     }
 
 
-    // Midi Eventhandler------------------------------------------------------------------------------------------------
-    public void noteOn(int channel, int pitch, int velocity) {
-        // Receive a noteOn
-        println();
-        println("Note On:");
-        println("--------");
-        println("Channel:" + channel);
-        println("Pitch:" + pitch);
-        println("Velocity:" + velocity);
-    }
 
-    public void noteOff(int channel, int pitch, int velocity) {
-        // Receive a noteOff
-        println();
-        println("Note Off:");
-        println("--------");
-        println("Channel:"+channel);
-        println("Pitch:"+pitch);
-        println("Velocity:"+velocity);
-    }
-
-    public void controllerChange(int channel, int number, int value) {
-        // Receive a controllerChange
-        println();
-        println("Controller Change:");
-        println("--------");
-        println("Channel:"+channel);
-        println("Number:"+number);
-        println("Value:"+value);
-
-        switch (channel){
-            case 0:
-                switch (number){
-                    case 20:
-                        switch (value){
-                            case 1:
-                                mBalls.ballsR += 20;
-                                break;
-                            case 127:
-                                mBalls.ballsR -= 20;
-                                break;
-                        }
-                        break;
-                    case 21:
-                        switch (value){
-                            case 1:
-                                indexPix += 1;
-                                println("intexPix: " +indexPix);
-                                break;
-                            case 127:
-                                indexPix -= 1;
-                                println("intexPix: " +indexPix);
-                                break;
-                        }
-                        break;
-
-                    case 16:
-                        mBalls.ballsR = 10 * value; // für carens meady board
-                        break;
-
-
-
-                }
-                break;
-        }
-
-    }
 
     public void keyPressed(){
         println("keyCode: " +keyCode);
@@ -185,6 +107,8 @@ public class RembrandMeta extends PApplet{
             case 40:
                 mBalls.ballsR -= 100;
                 break;
+
+                /*
             case 39:
                 indexPix += 1;
                 //println("intexPix: " +indexPix);
@@ -193,6 +117,8 @@ public class RembrandMeta extends PApplet{
                 indexPix -= 1;
                 //println("intexPix: " +indexPix);
                 break;
+
+                 */
         }
     }
 

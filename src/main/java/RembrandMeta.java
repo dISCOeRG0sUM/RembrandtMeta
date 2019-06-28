2c51c27d6d4d2a91517dcb6b931dc199577712ae
dISCOeRG0sUM <49689599+dISCOeRG0sUM@users.noreply.github.com>
2019-06-28 23:47:56 +0200

    brechung metafeld verändert

--- src/main/java/RembrandMeta.java
+++ src/main/java/RembrandMeta.java
@@ -61,7 +61,7 @@ public class RembrandMeta extends PApplet {
         // interesant OSC muss vor Syphone im setup stehen ansosten geht Syphone Client nicht.
         oscP5 = new OscP5(this, 9000);
         // Def die den OSC Emfpänger
-        myRemoteLocation = new NetAddress("172.16.42.156", 8000);
+        myRemoteLocation = new NetAddress("192.168.178.23", 8000);
 
 
         // GUI test
@@ -242,7 +242,7 @@ public class RembrandMeta extends PApplet {
         a.hover(mouseX, mouseY);
 
         // parameter update
-        mBalls.ballsR = ballSize * sizeM;
+        mBalls.ballsR = ballSize * sizeM * 500;
         a.mass = mass * 100;
 
 
